f377e12767cdd30cb85ef2d2c33d8fae271a164f
David <davidlee93@gmail.com>
2018-05-25 14:56:50 -0700

    cleaning up css

--- src/components/search-bar/search-bar.test.js
+++ src/components/search-bar/search-bar.test.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import { SearchBar } from "./index";
 
 describe("<SearchBar />", () => {
   it("Should render without crashing", () => {
     shallow(<SearchBar />);
+  });
+  it("Should keep the input when the form is submitted", () => {
+    const wrapper = mount(<GuessForm />);
+    const input = wrapper.find('input[type="text"]');
+    input.instance().value = "berkeley";
+    wrapper.simulate("submit");
+    expect(input.instance().value).toEqual("berkeley");
   });
 });
