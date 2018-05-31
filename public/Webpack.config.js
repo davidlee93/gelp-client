34347b61f61282bf24eb86f5cd79c6594d2e0e8e
David <davidlee93@gmail.com>
2018-05-31 11:26:33 -0700

    webpack7

--- public/Webpack.config.js
+++ public/Webpack.config.js
@@ -1,4 +1,4 @@
-const api_key = process.env.REACT_APP_WEBPACK_GOOGLE_API_KEY;
+const api_key = process.env.REACT_APP_GOOGLE_API_KEY;
 const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
@@ -6,7 +6,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       inject: false,
-      template: "./index.html",
+      template: "index.html",
 
       // Pass the full url with the key!
       apiUrl: `https://maps.googleapis.com/maps/api/js?key=${api_key}&libraries=places`
