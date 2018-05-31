4d8fe5f5d487abb0046fa49a5a0140830590f16b
David <davidlee93@gmail.com>
2018-05-31 10:15:14 -0700

    getting api key through webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,15 @@
+const api_key = process.env.GOOGLE_API_KEY;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: "./index.html",
+
+      // Pass the full url with the key!
+      apiUrl: `https://maps.googleapis.com/maps/api/js?key=${api_key}&libraries=places`
+    })
+  ]
+};
