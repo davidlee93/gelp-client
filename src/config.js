a2aacc97ad1f4b21362e3b68d3aa896d3811950f
David <davidlee93@gmail.com>
2018-05-31 09:02:48 -0700

    changing config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api";
+  process.env.REACT_APP_API_BASE_URL ||
+  "https://gelp-server.herokuapp.com/api" ||
+  "http://localhost:8080/api";
 export const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY || "";
