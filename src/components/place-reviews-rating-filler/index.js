3a39322c5f6c325f519f51317ba85859ec89bbcf
David <davidlee93@gmail.com>
2018-05-31 16:13:51 -0700

    readme update 1

--- src/components/place-reviews-rating-filler/index.js
+++ src/components/place-reviews-rating-filler/index.js
@@ -8,18 +8,18 @@ const RatingFiller = () => {
         <div className="place-result-review-filler">
           <div className="user-info-filler">
             <div className="user-name-filler">
-              <h5>David L.</h5>
+              <h5>Lorem I.</h5>
             </div>
             <div className="user-zip-member-filler">
-              <p>Zipcode: 94704</p>
+              <p>Zipcode: 90210</p>
               <p style={{ color: "#ff0000" }}>Member since `18</p>
             </div>
           </div>
           <div className="place-review-ratings-box-filler">
             <ul className="place-review-ratings-list-filler">
-              <li className="place-aggregate-rating-filler">Rating: 4</li>
-              <li>Quantity: 4</li>
-              <li>Quality: 3</li>
+              <li className="place-aggregate-rating-filler">Rating: 5</li>
+              <li>Quantity: 5</li>
+              <li>Quality: 5</li>
               <li>Pricing: 5</li>
             </ul>
             <p className="review-text-filler">
